909ad51c0bc5b40e6149033ec9d0966300afdbdf
datozhgenti <datozhgenti1998@gmail.com>
2022-04-21 23:32:39 +0400

    cart

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
 // alert("This website is not completed yet. I am working on that.");
+
+$(document).ready(() => {
+    $("nav li:last-child").on("click", () => {
+        $(".cart").toggle();
+    });
+
+    $(".fa-close").on("click", () => {
+        $(".cart").toggle();
+    });
+})
